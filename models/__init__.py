daf2775f21ed3a46695f17127d66d9222cec20c4
chris-solomou <Chris.Solomou@productmadness.com>
2025-11-28 12:49:11 +0100

    Update Models

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,29 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Dict, Any
+from typing import Dict, Any, List, Optional
 
 class Payload(BaseModel):
     user_id: int
     task :str
     params: Dict[str, Any]
-    
+
+class Metadata(BaseModel):
+    summaryType: int
+    customPrompt: Optional[str] = None
+    audienceContext: Optional[str] = None
+    includeScreenshots: bool
+    outputFormat: str
+    detailLevel: int
 
 class PubSubMessage(BaseModel):
     video_id: str
     summary_type: str
-    params: Dict[str, Any]
+    metadata: Dict[str, Any]
     processing_timestamp: datetime
+
+class SignedUrlResponse(BaseModel):
+    video_id: str
+    signed_url: str
+
+class SignedURLRequest(BaseModel):
+    files: List[Dict[str, str]]
