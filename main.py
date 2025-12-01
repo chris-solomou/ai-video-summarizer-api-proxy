bab86650cd139fe929ccd34b1dccc4aa5e7d49cc
chris-solomou <Chris.Solomou@productmadness.com>
2025-12-01 13:40:39 +0100

    Implemented endpoints for creating signed urls and sending messages to PubSub

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from fastapi.templating import Jinja2Templates
 from typing import Dict, Any, Optional, List, Union
 import uuid
 from datetime import datetime
-from models import PubSubMessage, SignedUrlResponse, SignedURLRequest, Metadata
+from models import PubSubMessage, SignedUrlResponse, SignedURLRequest, FormData
 from fastapi.staticfiles import StaticFiles
 from google.cloud import storage
-from sources.gcp import StorageBucket
+from sources.gcp import StorageBucket, PubSubPublisher
 
 
 def generate_video_id(file: Union[UploadFile, str]) -> str:
@@ -15,7 +15,7 @@ def generate_video_id(file: Union[UploadFile, str]) -> str:
     elif isinstance(file, str):
         file_name = file
     else:
-        raise TypeError(f"File of type {file} is not supported!")
+        raise TypeError(f"File of type {type(file)} is not supported!")
     ext = file_name.split(".")[-1]
     return str(uuid.uuid4()) + "." + ext
 
@@ -24,6 +24,8 @@ BUCKET_NAME = "ai-video-summarizer-dev-bucket"
 client = storage.Client()
 storage_bucket = StorageBucket(bucket_name=BUCKET_NAME, client=client)
 
+publisher = PubSubPublisher(pubsub_topic="topic")
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -31,36 +33,32 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def index(request: Request):
-    # await print(request.json())
+async def index(request: Request) -> Any:
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/generate-signed-urls")
-async def get_signed_urls(request: SignedURLRequest):
+async def get_signed_urls(request: SignedURLRequest) -> Dict[str, List[SignedUrlResponse]]:
     file_names = [file["filename"] for file in request.files]
 
     signed_urls = []
     try:
         for file in file_names:
             video_id = generate_video_id(file=file)
-            signed_url = storage_bucket.generate_dummy_signed_url(
-                blob_name=video_id
-            )
+            signed_url = storage_bucket.generate_dummy_signed_url(blob_name=video_id)
             signed_urls.append({"video_id": video_id, "signed_url": signed_url})
     except Exception as e:
         print(f"The folloing exception has occurred {e}!")
 
-    print(signed_urls)
     return {"files": signed_urls}
 
 
-@app.post("send-to-pubsub")
-def send_pubsub_message(message: Metadata): ...
-
-
-@app.post("/submit-metadata")
-async def submit_metadata(request: Request):
-    data = await request.json()  # or await request.json() if JSON
-    print(data)
-    return {"data": dict(data)}
+@app.post("/submit-form-data")
+async def submit_metadata(form_data: FormData) -> None:
+    data = form_data.model_dump()
+    video_ids = data.pop("video_ids")
+    for video_id in video_ids:
+        message = PubSubMessage(
+            video_id=video_id, metadata=data.copy(), processing_timestamp=datetime.now()
+        ).model_dump()
+        publisher.publish_message(message=message)
