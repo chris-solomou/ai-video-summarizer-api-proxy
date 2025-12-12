cec8213ba5228b39bb3c4f935ed81b4ae02dd97d
chris-solomou <Chris.Solomou@productmadness.com>
2025-12-12 08:33:42 +0100

    Google Authentication configs

---
+++ oauth.py
@@ -0,0 +1,13 @@
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
+
+config = Config(".env")
+oauth = OAuth(config)
+
+google = oauth.register(
+    name="google",
+    client_id=config("GOOGLE_CLIENT_ID"),
+    client_secret=config("GOOGLE_CLIENT_SECRET"),
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
+)
